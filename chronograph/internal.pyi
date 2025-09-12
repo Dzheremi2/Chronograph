bcc2bc14bb07d32ce2447dc7e2893139f56f1007
Dzheremi <chichvarov1315@gmail.com>
2025-09-13 00:02:30 +0300

    chore: bump version of `blueprint-compiler` to `v0.18.0`
    chore: re-declared one-time-used menus to be inline as `blueprint-compiler` now allows it along with GtkBuilder
    
    chore: added stubs for dynamic variables generated by dgutils for `internals.py:Constants`

--- chronograph/internal.pyi
+++ chronograph/internal.pyi
@@ -1,7 +1,32 @@
 # pylint: disable=all
 
-from dgutils import Constants as Const
+from io import TextIOWrapper
+from logging import Logger
+from pathlib import Path
+from typing import Final
+
+from gi.repository import Gdk
+
+from chronograph.main import ChronographApplication
+from chronograph.window import ChronographWindow
 from dgutils import Schema as Sch
 
+class _Constants:
+    APP_ID: Final[str]
+    VERSION: Final[str]
+    PREFIX: Final[str]
+    CACHEV: Final[int]
+
+    APP: Final[ChronographApplication]
+    WIN: Final[ChronographWindow]
+    CACHE_FILE: Final[TextIOWrapper]
+    CACHE: dict
+
+    CFG_DIR: Final[Path]
+    DATA_DIR: Final[Path]
+    CACHE_DIR: Final[Path]
+    COVER_PLACEHOLDER: Final[Gdk.Texture]
+    LOGGER: Logger
+
+Constants: _Constants
 Schema: Sch
-Constants: Const
