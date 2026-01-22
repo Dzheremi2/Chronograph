caa88429be8eae326bd0a1652d13a1cd326e6960
Dzheremi <chichvarov1315@gmail.com>
2026-01-22 18:59:24 +0300

    chore: some dgutils changes

--- dgutils/__init__.py
+++ dgutils/__init__.py
@@ -1,7 +1,6 @@
 from dgutils.actions import Actions
-from dgutils.constants import Constants
 from dgutils.linker import Linker
 from dgutils.schema import Schema
 from dgutils.singleton import GSingleton, Singleton
 
-__all__ = ["Actions", "Constants", "GSingleton", "Linker", "Schema", "Singleton"]
+__all__ = ["Actions", "GSingleton", "Linker", "Schema", "Singleton"]
